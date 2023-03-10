bb97a856d5010a3a5e819ff8c858f6bb7f84fac0
LiYanLance <liyan1924@gmail.com>
2023-03-10 10:26:32 +0800

    Add uuid support for request id

--- libs/ur-registry-ffi/src/solana/solana_signarure.rs
+++ libs/ur-registry-ffi/src/solana/solana_signarure.rs
@@ -2,6 +2,7 @@ use hex;
 use serde_json::json;
 use ur_registry::solana::sol_signature::SolSignature;
 use ur_registry::traits::{From};
+use uuid::Uuid;
 
 use crate::export;
 
@@ -14,7 +15,8 @@ export! {
         let res = serde_cbor::from_slice(cbor.as_slice()).unwrap();
         let sol_signature = SolSignature::from_cbor(res).unwrap();
 
-        let request_id = hex::encode(sol_signature.get_request_id().unwrap());
+        let uuid_hex = hex::encode(sol_signature.get_request_id().unwrap());
+        let request_id = Uuid::parse_str(&uuid_hex).unwrap().to_string();
         let signature = hex::encode(sol_signature.get_signature());
 
         let john = json!({
@@ -34,7 +36,7 @@ mod tests {
     #[test]
     fn test_parse() {
         let sol_signature_cbor = "a201d825509b1deb4d3b7d4bad9bdd2b0d7b3dcb6d025840d4f0a7bcd95bba1fbb1051885054730e3f47064288575aacc102fbbf6a9a14daa066991e360d3e3406c20c00a40973eff37c7d641e5b351ec4a99bfe86f335f7";
-        let expect_result = "{\"request_id\":\"9b1deb4d3b7d4bad9bdd2b0d7b3dcb6d\",\"signature\":\"d4f0a7bcd95bba1fbb1051885054730e3f47064288575aacc102fbbf6a9a14daa066991e360d3e3406c20c00a40973eff37c7d641e5b351ec4a99bfe86f335f7\"}";
+        let expect_result = "{\"request_id\":\"9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d\",\"signature\":\"d4f0a7bcd95bba1fbb1051885054730e3f47064288575aacc102fbbf6a9a14daa066991e360d3e3406c20c00a40973eff37c7d641e5b351ec4a99bfe86f335f7\"}";
 
         assert_eq!(expect_result, parse_sol_signature(sol_signature_cbor));
     }
